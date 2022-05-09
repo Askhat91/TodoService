7b2ee7d8525e4679bcbc52db1d2923fd7ddf9df3
Askhat91 <59979874+Askhat91@users.noreply.github.com>
2022-05-09 21:06:27 +0600

    added sql express edition

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
+            string connection = Configuration.GetConnectionString("TodoContextConnection");
+            services.AddDbContext<TodoContext>(options => options.UseSqlServer(connection));
 
             services.AddControllers();
 
